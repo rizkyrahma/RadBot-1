1a2c8d1a56ad28481d6781874a9531c6dbd194f1
Nur Alam <99713260+rizkyrahma@users.noreply.github.com>
2022-03-10 00:52:40 +0700

    Update yvebot.js

--- plugins/yvebot.js
+++ plugins/yvebot.js
@@ -3,8 +3,8 @@ let PhoneNumber = require('awesome-phonenumber')
 let levelling = require('../lib/levelling')
   
 let handler = async (m, { conn }) => {
-let caption = `*HALO Bang*\n*Saya RadBotZ*\n*Pilih Menu Di Bawah Yaa Bree*\n*Spam = Banned*\n*Telp = Blok*\n\n*Join Group WhatsApp*\nhttps://chat.whatsapp.com/LM77ornJmDz2tMIkEIYKGe\n\n*Untuk Melihat Menu Bot Ketik .menu*`.trim()
-conn.send3Button(m.chat, caption, `Powered By Baileys\nCreated RadBotZ By Raditya\n`.trim(), '📑MENU', '.?', '🍭Owner', '.owner', '📖Info Bot', '.infobot', m)
+let caption = `*HALO Bang*\n*Saya AlamBotz*\n*Pilih Menu Di Bawah Yaa Bree*\n*Spam = Banned*\n*Telp = Blok*\n\n*Join Group WhatsApp*\nhttps://chat.whatsapp.com/LM77ornJmDz2tMIkEIYKGe\n\n*Untuk Melihat Menu Bot Ketik .menu*`.trim()
+conn.send3Button(m.chat, caption, `Powered By Baileys\nCreated AlamBotz By Raditya\n`.trim(), '📑MENU', '.?', '🍭Owner', '.owner', '📖Info Bot', '.infobot', m)
 }
 
 handler.command = /^(hel)$/i
