81cfdece88433cc5f32c674751d12a160fae42bd
Nur Alam <99713260+rizkyrahma@users.noreply.github.com>
2022-03-10 10:33:42 +0700

    Update group-welcome.js

--- plugins/group-welcome.js
+++ plugins/group-welcome.js
@@ -1,9 +1,9 @@
 let moment = require('moment-timezone')
 let fs = require('fs')
 let handler = async (m, { conn, command, text }) => {
-let img = './src/RadBot.png'
+let img = './src/AlamBotz.png'
 let user = global.db.data.users[m.sender]
-conn.send2Button(m.chat, `Silahkan pilih button dibawah untuk mengaktifkan fitur welcome`, 'RadBotZ ×͜×', 'On', '#on welcome', 'Off', '#off welcome', { key: { fromMe: false, remoteJid: '6283136505591-1614953337@g.us', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 2022, thumbnail: fs.readFileSync('./src/RadBotZ.jpg')
+conn.send2Button(m.chat, `Silahkan pilih button dibawah untuk mengaktifkan fitur welcome`, 'AlamBotz ×͜×', 'On', '#on welcome', 'Off', '#off welcome', { key: { fromMe: false, remoteJid: '6283136505591-1614953337@g.us', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 2022, thumbnail: fs.readFileSync('./src/AlamBotz.jpg')
 }}})
 }
 handler.command = /^welcome$/i
