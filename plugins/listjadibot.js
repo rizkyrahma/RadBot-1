78cc80d8a02f20a4222d8f890463064e6f105af8
Nur Alam <99713260+rizkyrahma@users.noreply.github.com>
2022-03-12 10:53:41 +0700

    Update listjadibot.js

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -1,9 +1,8 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply(users.map(v => 'wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+  m.reply( `🌟 *Bot Utama :*\n⤷ wa.me/6285888083107?text=.menu (KannaBot)\n\n`+ `🤖 *JadiBot :*\n` + users.map(v => '✎ wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
 }
-handler.command = handler.help = ['listjadibot']
+handler.command = handler.help = ['listjadibot','listbot','ljb']
 handler.tags = ['jadibot']
-handler.owner = false
 
 module.exports = handler
